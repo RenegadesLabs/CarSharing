37df887c3044af49f9ee62fce0aa93c43c41ba13
Victor <victor.freeman86@gmail.com>
2017-10-18 18:57:10 +0300

    bottom nav implemented

--- app/src/main/java/android/cardee/com/cardee/owner_home/view/OwnerHomeActivity.java
+++ app/src/main/java/android/cardee/com/cardee/owner_home/view/OwnerHomeActivity.java
@@ -1,5 +1,6 @@
 package android.cardee.com.cardee.owner_home.view;
 
+import android.cardee.com.cardee.R;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +11,7 @@ public class OwnerHomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner_home);
     }
 
     @Override
