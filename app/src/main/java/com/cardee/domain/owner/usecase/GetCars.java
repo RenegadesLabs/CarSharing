89a9345c2d3737abc92a60ecc9270412e5d3d74e
Victor <victor.freeman86@gmail.com>
2017-10-19 17:38:58 +0300

    Added base usecases

---
+++ app/src/main/java/com/cardee/domain/owner/usecase/GetCars.java
@@ -0,0 +1,14 @@
+package com.cardee.domain.owner.usecase;
+
+import com.cardee.domain.UseCase;
+
+public class GetCars implements UseCase {
+
+
+    @Override
+    public void execute() {
+
+    }
+
+
+}
---
+++ app/src/main/java/com/cardee/domain/renter/usecase/GetCars.java
@@ -0,0 +1,8 @@
+package com.cardee.domain.renter.usecase;
+
+/**
+ * Created by mac on 19.10.2017.
+ */
+
+public class GetCars {
+}
