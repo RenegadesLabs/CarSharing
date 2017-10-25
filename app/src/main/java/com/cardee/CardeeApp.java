c089a703afe1d3ea4bedcc56b4c337454e5f633d
Victor <victor.freeman86@gmail.com>
2017-10-25 17:36:01 +0300

    Added Auth token hendling implementation

--- app/src/main/java/com/cardee/CardeeApp.java
+++ app/src/main/java/com/cardee/CardeeApp.java
@@ -3,13 +3,23 @@ package com.cardee;
 import android.app.Application;
 import android.content.Context;
 
+import com.cardee.data_source.remote.api.client.HttpClientProvider;
+
+import retrofit2.Retrofit;
+
 public class CardeeApp extends Application {
 
     public static Context context;
+    public static Retrofit retrofit;
 
     @Override
     public void onCreate() {
         super.onCreate();
         context = this;
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BuildConfig.BASE_URL)
+                .client(HttpClientProvider.newInstance().provide(this))
+                .build();
     }
 }
---
+++ app/src/main/java/com/cardee/data_source/remote/service/AccountManager.java
@@ -0,0 +1,49 @@
+package com.cardee.data_source.remote.service;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class AccountManager {
+
+    private static final String AUTH_TOKEN_STORE = "_auth_token_store";
+    private static final String AUTH_TOKEN = "_auth_token";
+    private static final String AUTH_HEADER_NAME = "Authorization";
+    private static final String DEFAULT_AUTH_TOKEN = "";
+
+    private static AccountManager INSTANCE;
+
+    private SharedPreferences mPrefs;
+
+    private AccountManager(Context context) {
+        mPrefs = context.getSharedPreferences(AUTH_TOKEN_STORE, Context.MODE_PRIVATE);
+    }
+
+    public Request modifyRequestHeaders(Request request) {
+        Request.Builder builder = request.newBuilder();
+        builder.addHeader(AUTH_HEADER_NAME, mPrefs.getString(AUTH_TOKEN, DEFAULT_AUTH_TOKEN));
+        return builder.build();
+    }
+
+    public void handleResponseHeaders(Response response) {
+        String headerValue = response.header(AUTH_HEADER_NAME);
+        if (headerValue != null) {
+            mPrefs.edit().putString(AUTH_TOKEN, headerValue).apply();
+        }
+    }
+
+    public void onLogout() {
+        mPrefs.edit().remove(AUTH_TOKEN).apply();
+    }
+
+    public static AccountManager getInstance(@NonNull Context context) {
+        if (INSTANCE == null) {
+            INSTANCE = new AccountManager(context);
+        }
+        return INSTANCE;
+    }
+}
