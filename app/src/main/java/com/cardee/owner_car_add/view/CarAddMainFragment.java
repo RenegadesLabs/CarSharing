ad7ed93eb58d2c300934e3135df329fc64cc8fc9
David Ryabokonenko <davrbk@gmail.com>
2017-11-06 12:05:45 +0200

    Enable to set first item in add-car feature (just in view).

--- app/src/main/java/com/cardee/owner_car_add/view/CarAddMainFragment.java
+++ app/src/main/java/com/cardee/owner_car_add/view/CarAddMainFragment.java
@@ -3,6 +3,8 @@ package com.cardee.owner_car_add.view;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.widget.AppCompatImageView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,8 +12,6 @@ import android.widget.TextView;
 
 import com.cardee.R;
 import com.cardee.owner_car_add.view.items.CarAddItemFragment;
-
-import org.w3c.dom.Text;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,25 +25,88 @@ public class CarAddMainFragment extends Fragment {
 
     private CarAddView mView;
 
+    private String mValueItem1, mValueItem2,
+            mValueItem3, mValueItem4, mValueItem5,
+            mValueItem6;
+
+    private boolean isFilled1 = false;
+
+    private boolean isFilled2, isFilled3,
+            isFilled4, isFilled5, isFilled6;
+
     @BindView(R.id.tv_addCarItem1)
     public TextView addCarItem1TV;
+
+    @BindView(R.id.tv_addCarItem2)
+    public TextView addCarItem2TV;
+
+    @BindView(R.id.tv_addCarItem3)
+    public TextView addCarItem3TV;
+
+    @BindView(R.id.tv_addCarItem4)
+    public TextView addCarItem4TV;
+
+    @BindView(R.id.tv_addCarItem5)
+    public TextView addCarItem5TV;
+
+    @BindView(R.id.tv_addCarItem6)
+    public TextView addCarItem6TV;
+
+    @BindView(R.id.iv_addCarItem1)
+    public AppCompatImageView addCarItem1IV;
+
+    @BindView(R.id.iv_addCarItem2)
+    public AppCompatImageView addCarItem2IV;
+
+    @BindView(R.id.iv_addCarItem3)
+    public AppCompatImageView addCarItem3IV;
+
+    @BindView(R.id.iv_addCarItem4)
+    public AppCompatImageView addCarItem4IV;
+
+    @BindView(R.id.iv_addCarItem5)
+    public AppCompatImageView addCarItem5IV;
+
+    @BindView(R.id.iv_addCarItem6)
+    public AppCompatImageView addCarItem6IV;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_add_car_main, container, false);
         mUnbinder = ButterKnife.bind(this, v);
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
 
         Bundle args = getArguments();
         if (args != null) {
-            switch(args.getInt(CarAddItemFragment.FRAGMENT_NUMBER)) {
+            switch (args.getInt(CarAddItemFragment.FRAGMENT_NUMBER)) {
                 case 0:
-                    addCarItem1TV.setText(args.getString(CarAddItemFragment.FRAGMENT_VALUE));
+                    mValueItem1 = args.getString(CarAddItemFragment.FRAGMENT_VALUE);
+                    addCarItem1TV.setText(mValueItem1);
+                    addCarItem1IV.setImageResource(R.drawable.ic_check_circle);
+                    isFilled1 = true;
                     break;
             }
+        } else {
+            initViewState();
         }
 
         return v;
+    }
+
+    private void initViewState() {
+        if (mValueItem1 != null && !mValueItem1.equals("")
+                && isFilled1) {
+            addCarItem1TV.setText(mValueItem1);
+            addCarItem1IV.setImageResource(R.drawable.ic_check_circle);
+        }
+
     }
 
     public void setViewListener(CarAddView viewListener) {
--- app/src/main/java/com/cardee/owner_car_add/view/items/CarAddItem2Fragment.java
+++ app/src/main/java/com/cardee/owner_car_add/view/items/CarAddItem2Fragment.java
@@ -8,14 +8,33 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.cardee.R;
+import com.cardee.owner_car_add.view.CarAddActivity;
 
-public class CarAddItem2Fragment extends CarAddItemFragment {
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+public class CarAddItem2Fragment extends CarAddItemFragment implements CarAddActivity.CarAddActionListener {
+
+    private Unbinder mUnbinder;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_add_car_item, container, false);
+        View v = inflater.inflate(R.layout.fragment_add_car_item2, container, false);
+        mUnbinder = ButterKnife.bind(this, v);
 
+        ((CarAddActivity) getActivity()).setActionListener(this);
         return v;
+    }
+
+    @Override
+    public void onSaveClicked() {
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mUnbinder.unbind();
     }
 }
