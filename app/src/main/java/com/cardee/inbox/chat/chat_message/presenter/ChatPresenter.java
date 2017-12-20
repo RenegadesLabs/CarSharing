c96820eea38c1c39be4402df86a922c6326a8f12
renegadeslabs <vv.enkoff@gmail.com>
2017-12-20 12:06:07 +0200

    chat message added;

---
+++ app/src/main/java/com/cardee/inbox/chat/chat_message/presenter/ChatPresenter.java
@@ -0,0 +1,12 @@
+package com.cardee.inbox.chat.chat_message.presenter;
+
+import com.cardee.inbox.chat.chat_message.view.ChatView;
+
+public class ChatPresenter {
+
+    private ChatView mView;
+
+    public ChatPresenter(ChatView view) {
+        mView = view;
+    }
+}
---
+++ app/src/main/java/com/cardee/inbox/chat/chat_message/view/ChatView.java
@@ -0,0 +1,7 @@
+package com.cardee.inbox.chat.chat_message.view;
+
+import com.cardee.mvp.BaseView;
+
+public interface ChatView extends BaseView {
+
+}
