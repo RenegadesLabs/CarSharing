284a2f0069cc7557f930eddd207b927af43b56fa
David Ryabokonenko <davrbk@gmail.com>
2018-01-17 17:55:27 +0200

    Minor addings

--- app/src/main/java/com/cardee/renter_browse_cars/presenter/RenterBrowseCarListPresenter.java
+++ app/src/main/java/com/cardee/renter_browse_cars/presenter/RenterBrowseCarListPresenter.java
@@ -1,4 +1,57 @@
 package com.cardee.renter_browse_cars.presenter;
 
-public class RenterBrowseCarListPresenter {
+
+import com.cardee.data_source.Error;
+import com.cardee.domain.UseCaseExecutor;
+
+import io.reactivex.functions.Consumer;
+
+public class RenterBrowseCarListPresenter implements Consumer<RenterBrowseCarListContract.CarEvent> {
+
+    private final UseCaseExecutor mExecutor;
+    private RenterBrowseCarListContract.View mView;
+
+    private boolean firstStart = true;
+
+
+    public RenterBrowseCarListPresenter(RenterBrowseCarListContract.View view) {
+        mView = view;
+        mExecutor = UseCaseExecutor.getInstance();
+    }
+
+    @Override
+    public void accept(RenterBrowseCarListContract.CarEvent carEvent) throws Exception {
+
+        switch (carEvent.getAction()) {
+            case UPDATED:
+                break;
+
+            case OPEN:
+                break;
+
+            case ADD_TO_FAVORITE:
+                break;
+        }
+
+    }
+
+    public void loadItems() {
+        if (firstStart && mView != null) {
+            mView.showProgress(true);
+        }
+
+//        mExecutor.execute();
+
+    }
+
+    private void handleError(Error error) {
+
+        if (error.isAuthError()) {
+            mView.onUnauthorized();
+        } else if (error.isConnectionError()) {
+            mView.onConnectionLost();
+        } else {
+            mView.showMessage(error.getMessage());
+        }
+    }
 }
