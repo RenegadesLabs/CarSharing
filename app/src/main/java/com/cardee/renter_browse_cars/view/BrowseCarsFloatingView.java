ff47c33e8198a7325b7ca2eeb432badee81ac2a8
David Ryabokonenko <davrbk@gmail.com>
2018-01-17 12:55:42 +0200

    Some view improvements.

---
+++ app/src/main/java/com/cardee/renter_browse_cars/view/BrowseCarsFloatingView.java
@@ -0,0 +1,27 @@
+package com.cardee.renter_browse_cars.view;
+
+import android.content.Context;
+import android.support.constraint.ConstraintLayout;
+import android.util.AttributeSet;
+
+
+public class BrowseCarsFloatingView extends ConstraintLayout {
+
+
+    public BrowseCarsFloatingView(Context context) {
+        super(context);
+    }
+
+    public BrowseCarsFloatingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BrowseCarsFloatingView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+    }
+}
