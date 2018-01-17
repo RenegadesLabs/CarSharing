f25e4d360092090eca0c1747f2f3f2c3a361b573
renegadeslabs <vv.enkoff@gmail.com>
2018-01-17 18:36:07 +0200

    cars filter data binding implemented;

--- app/src/main/java/com/cardee/domain/renter/entity/BrowseCarsFilter.kt
+++ app/src/main/java/com/cardee/domain/renter/entity/BrowseCarsFilter.kt
@@ -6,13 +6,15 @@ import com.cardee.BR
 import com.cardee.CardeeApp
 import com.cardee.R
 
-class BrowseCarsFilter(vehicleTypeId: Byte = 0,
-                       val vehicleTypeDesc: Array<String> = arrayOf(CardeeApp.context.resources.getString(R.string.vehicle_type_personal_cars_desc)),
+class BrowseCarsFilter(vehicleTypeId: Int = 0,
+                       val vehicleTypeDesc: Array<String> = arrayOf(CardeeApp.context.resources.getString(R.string.vehicle_type_personal_cars_desc),
+                               CardeeApp.context.resources.getString(R.string.vehicle_type_private_desc),
+                               CardeeApp.context.resources.getString(R.string.vehicle_type_commercial_desc)),
                        val bookingHourly: Boolean = false) : BaseObservable() {
 
 
     @get:Bindable
-    var vehicleTypeId: Byte = 0
+    var vehicleTypeId: Int = 0
         set(value) {
             field = value
             notifyPropertyChanged(BR.vehicleTypeId)
--- app/src/main/java/com/cardee/renter_browse_cars/view/filter/view/FilterActivity.kt
+++ app/src/main/java/com/cardee/renter_browse_cars/view/filter/view/FilterActivity.kt
@@ -2,6 +2,7 @@ package com.cardee.renter_browse_cars.view.filter.view
 
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.design.widget.TabLayout
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 import com.cardee.R
@@ -11,17 +12,33 @@ import com.cardee.domain.renter.entity.BrowseCarsFilter
 class FilterActivity : AppCompatActivity(), FilterView {
 
     private var mCurrentToast: Toast? = null
+    lateinit var binding: ActivityFilterBinding
+    lateinit var filter: BrowseCarsFilter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding: ActivityFilterBinding = DataBindingUtil.setContentView(this, R.layout.activity_filter)
-
-        val filter = BrowseCarsFilter()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_filter)
+        filter = BrowseCarsFilter()
         binding.filter = filter
         binding.executePendingBindings()
+        binding.vehicleType.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when (tab?.position) {
+                    0 -> filter.vehicleTypeId = 0
+                    1 -> filter.vehicleTypeId = 1
+                    2 -> filter.vehicleTypeId = 2
+                }
+            }
 
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+        })
     }
+
 
     override fun showProgress(show: Boolean) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
