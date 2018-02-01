9b2fa10168bf85ca9479004e4d573e1d2db4463f
Roman Laktionov <r.a.laktionov@gmail.com>
2018-02-01 18:30:26 +0200

    Started with implementing Renter car Details Screen.

---
+++ app/src/main/java/com/cardee/renter_car_details/view/RenterCarDetailsActivity.kt
@@ -0,0 +1,15 @@
+package com.cardee.renter_car_details.view
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+
+class RenterCarDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+    }
+
+}
