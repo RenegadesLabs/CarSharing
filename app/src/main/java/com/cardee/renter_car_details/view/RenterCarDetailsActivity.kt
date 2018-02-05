09eecee47357332c9beb9699ae690ed987335242
Roman Laktionov <r.a.laktionov@gmail.com>
2018-02-05 15:47:55 +0200

    Some addings.

--- app/src/main/java/com/cardee/renter_car_details/view/RenterCarDetailsActivity.kt
+++ app/src/main/java/com/cardee/renter_car_details/view/RenterCarDetailsActivity.kt
@@ -3,13 +3,15 @@ package com.cardee.renter_car_details.view
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import com.cardee.R
+import kotlinx.android.synthetic.main.activity_renter_car_details.*
 
 
 class RenterCarDetailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
-
+        setContentView(R.layout.activity_renter_car_details)
     }
 
 }
