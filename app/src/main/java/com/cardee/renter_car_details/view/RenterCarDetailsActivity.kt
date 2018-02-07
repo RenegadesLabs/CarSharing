049f7ab0d96b0717ad003b2be37d24147ee69b58
Victor <victor.freeman86@gmail.com>
2018-02-07 12:15:22 +0200

    Minor fixes

--- app/src/main/java/com/cardee/renter_car_details/view/RenterCarDetailsActivity.kt
+++ app/src/main/java/com/cardee/renter_car_details/view/RenterCarDetailsActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
 import android.view.View
-import android.widget.Toast
 import com.cardee.R
 import com.cardee.renter_book_car.view.BookCarActivity
 import com.cardee.renter_browse_cars_map.LocationClient
--- app/src/main/java/com/cardee/util/ui/LockableView.kt
+++ app/src/main/java/com/cardee/util/ui/LockableView.kt
@@ -1,8 +1,5 @@
 package com.cardee.util.ui
 
-/**
- * Created by mac on 06.02.2018.
- */
 interface LockableView {
 
     fun lock()
