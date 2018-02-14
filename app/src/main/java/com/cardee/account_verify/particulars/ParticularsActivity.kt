b6d413ce3af137397158f0071779108ef29c0899
renegadeslabs <vv.enkoff@gmail.com>
2018-02-14 19:04:18 +0200

    small fixes;

--- app/src/main/java/com/cardee/account_verify/particulars/ParticularsActivity.kt
+++ app/src/main/java/com/cardee/account_verify/particulars/ParticularsActivity.kt
@@ -1,14 +1,19 @@
 package com.cardee.account_verify.particulars
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
+import android.widget.DatePicker
 import android.widget.Toast
 import com.cardee.R
 import com.cardee.mvp.BaseView
-import kotlinx.android.synthetic.main.activity_verify_account.*
+import kotlinx.android.synthetic.main.activity_particulars.*
 
-class ParticularsActivity : AppCompatActivity(), BaseView {
+class ParticularsActivity : AppCompatActivity(), BaseView, DatePickerDialog.OnDateSetListener {
+    override fun onDateSet(p0: DatePicker?, p1: Int, p2: Int, p3: Int) {
+
+    }
 
     private var mCurrentToast: Toast? = null
 
@@ -16,6 +21,10 @@ class ParticularsActivity : AppCompatActivity(), BaseView {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_particulars)
         initToolBar()
+        birthDateInput.setOnClickListener {
+            val dialog = DatePickerDialog(this, this, 2018, 1, 1)
+            dialog.show()
+        }
     }
 
     private fun initToolBar() {
