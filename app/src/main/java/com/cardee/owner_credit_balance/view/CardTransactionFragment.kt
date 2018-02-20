47bd8de58bc889e0b4c02841db79e60b4214e5e9
Victor <victor.freeman86@gmail.com>
2018-02-20 18:30:34 +0200

    CardTransfer base UI implemented

--- app/src/main/java/com/cardee/owner_credit_balance/view/CardTransactionFragment.kt
+++ app/src/main/java/com/cardee/owner_credit_balance/view/CardTransactionFragment.kt
@@ -1,6 +1,11 @@
 package com.cardee.owner_credit_balance.view
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.cardee.R
 
 
 class CardTransactionFragment : Fragment() {
@@ -11,5 +16,17 @@ class CardTransactionFragment : Fragment() {
 
             return CardTransactionFragment()
         }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.fragment_card_transfer, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
